048854e8388448c4f41e8c1f1efde2a7bd4404a6
Massimo <rabuffo.massimo@gmail.com>
2023-11-06 18:10:56 +0100

    exercise_4

---
+++ src/exercise-4-type-aliases-start.ts
@@ -0,0 +1,18 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Create a `Country` type alias for the `string` type.
+// Change the type for each variable to use the `Country` type.
+
+type Country = string;
+
+const country1: Country = "Nigeria";
+
+const country2: Country = "Italy";
+
+const country3: Country = "China";
+
+// ----
+
+export {};
