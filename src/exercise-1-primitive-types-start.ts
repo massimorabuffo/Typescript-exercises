cc13a325173b6d3af91f535a4ba9080208ae493f
Massimo <rabuffo.massimo@gmail.com>
2023-11-06 17:05:35 +0100

    exercise_1

---
+++ src/exercise-1-primitive-types-start.ts
@@ -0,0 +1,27 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Add type annotations to each of these variables.
+
+let country: string = "Italy";
+
+let year: undefined = undefined;
+
+let averageTemperature: number = 23.6;
+
+let visited: boolean = true;
+
+let currency: null = null;
+
+// Change the type annotations on these variables so they are correct.
+
+let population: number = 47_450_795;
+
+let isSummer: boolean = false;
+
+let languages: string = "Spanish, English";
+
+// ----
+
+export {};
